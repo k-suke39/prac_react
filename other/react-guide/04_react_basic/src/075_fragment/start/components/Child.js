a45f010a06697b60127936bec76a3f231176dcd8
k-suke39 <pz.mozarla@gmail.com>
2023-07-17 09:38:36 +0900

    first

---
+++ other/react-guide/04_react_basic/src/075_fragment/start/components/Child.js
@@ -0,0 +1,15 @@
+import "./Child.css";
+
+const Child = () => {
+  return (
+    <>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Component</h3>
+      <p>Lorem hellonvfrenbrnbsenv</p>
+    </>
+  );
+};
+
+export default Child;
