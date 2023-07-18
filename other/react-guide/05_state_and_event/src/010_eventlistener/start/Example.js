836a358ff421ad6ea2d64e335c673d35d0b40f4d
k-suke39 <pz.mozarla@gmail.com>
2023-07-18 11:24:16 +0900

    ⚡️

--- other/react-guide/05_state_and_event/src/010_eventlistener/start/Example.js
+++ other/react-guide/05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,12 @@
 const Example = () => {
+  const eventHandler = () => {
+    alert("クリックされました");
+  };
   return (
-    <></>
+    <>
+      <button onClick={eventHandler}>クリックしてね</button>
+      <button>クリックしてね</button>
+    </>
   );
 };
 
