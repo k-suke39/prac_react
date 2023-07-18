7952f26ef8287e9d6342fc301ac559c70b15c42f
k-suke39 <pz.mozarla@gmail.com>
2023-07-18 11:13:19 +0900

    f

---
+++ other/react-guide/04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -0,0 +1,10 @@
+const Expression = () => {
+  const title = "Expression";
+  return (
+    <div className="expression">
+      <h3>Hello {title}</h3>
+    </div>
+  );
+};
+
+export default Expression;
